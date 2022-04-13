2e622f8e38cb2fb0fac8e884b93ca93e23733d1f
Ning <nyan5547@uni.sydney.edu.au>
2022-04-13 21:38:19 +0800

    Axios GET

--- client/js/index.js
+++ client/js/index.js
@@ -1,10 +1,26 @@
 
-function test() {
+function jQueryAJAXTest() {
     $(document).ready(function(){
         $("button").click(function(){
             $.get({url:"http://localhost:3000/user",success:function(result){
-                    $("#div1").html(result['name']);
+                    $("#div1").html(result['Name']);
                 }});
         });
     });
+}
+
+function axiosTest() {
+    axios.get('http://localhost:3000/user')
+        .then(function (response) {
+            //native javascript
+            // var div1 = document.getElementById('div1');
+            // div1.innerHTML = response.data['Name'];
+
+            //jQuery
+            $("#div1").html(response.data['Name']);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
 }
