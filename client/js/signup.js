d18ba32ffb8bd37fc404651417ac80d5ef7fe7cf
rtan9055 <rtan9055@uni.sydney.edu.au>
2022-05-03 11:51:50 +0800

    signin/signup 2nd-spa

--- client/js/signup.js
+++ client/js/signup.js
@@ -1,6 +1,26 @@
-// for sign up
-$(document).ready(function() {
-    $("#signupbtn").on("click",function() {
+$(document).ready(function () {
+    $('#loginForm').hide()
+    $('#signupForm').show()
+
+    $('#toSignUp').click(function () {
+        toSignUp()
+    })
+
+    $('#toLogin').click(function () {
+        toLogin()
+    })
+
+    function toLogin() {
+        $('#loginForm').show()
+        $('#signupForm').hide()
+    }
+
+    function toSignUp() {
+        $('#loginForm').hide()
+        $('#signupForm').show()
+    }
+
+    $("#signupbtn").on("click", function () {
         var firstname = $("#first_name").val();
         var lastname = $("#last_name").val();
         var email = $("#email").val();
@@ -13,52 +33,147 @@ $(document).ready(function() {
         var pwdInput = $("#pwdInput");
         var cpwdInput = $("#cpwdInput");
 
+        // Input field has empty value
+        if (!firstname || !lastname || !email || !pwd || !cpwd) {
+            if (!firstname) {
+                firstnameInput.text("Filed must not be empty");
+            }
+            if (!lastname) {
+                lastnameInput.text("Filed must not be empty");
+            }
+            if (!email) {
+                emailInput.text("Filed must not be empty");
+            }
+            if (!pwd) {
+                pwdInput.text("Filed must not be empty");
+            }
+            if (!cpwd) {
+                cpwdInput.text("Filed must not be empty");
+            }
+            return false;
+        }
+
         const data = {
             firstname: firstname,
-            lastname:lastname,
-            email:email,
-            pwd:pwd,
-            cpwd:cpwd
+            lastname: lastname,
+            email: email,
+            pwd: pwd,
+            cpwd: cpwd
         }
+
+
         axios.post('http://localhost:3000/api/user/signup', data)
             .then(function (response) {
                 var error = response.data['error'];
+
+                if (response.data['success']) {
+                    alert('Sign up successfully, you can login now');
+                    toLogin()
+                    return false
+                }
+
                 firstnameInput.text("");
                 lastnameInput.text("");
                 emailInput.text("");
                 pwdInput.text("");
                 cpwdInput.text("");
 
-                if (error) {
-                    if (firstname == "") {
-                        firstnameInput.text(error.firstname);
-                    }
-                    if (lastname == "") {
-                        lastnameInput.text(error.lastname);
-                    }
-                    if (email == "") {
-                        emailInput.text(error.email);
-                    }
-                    if (pwd == "") {
-                        pwdInput.text(error.password);
-                    }
-                    if (cpwd == "" || pwd != cpwd) {
-                        cpwdInput.text(error.pwdConfirm);
-                    }
-                    // 可能还需要加
+                if (('pwdConfirm' in error)) {
+                    cpwdInput.text(error['pwdConfirm']);
+                }
+
+                if (('password' in error)) {
+                    pwdInput.text(error['possword']);
+                }
+
+                if (('email' in error)) {
+                    emailInput.text(error['email']);
+                }
+
+                if (('fail' in error)) {
+                    emailInput.text(error['fail']);
                 }
 
                 if (response.data['success']) {
-                    // alert 会优先执行并且阻塞页面渲染
-                    setTimeout(function () {
-                        alert(response.data['success']);
-                        window.location.href = "Signin.html";
-                    }, 1000);
-                    console.log("success - server")
+                    alert('Sign up successfully, you can login now');
+                    toLogin()
                 }
             })
             .catch(function (error) {
                 console.log(error);
             });
     });
+
+    function clearAll() {
+        $('#first_name').text("");
+        $('#last_name').text("");
+        $('#email').text("");
+        $('#pwd').text("");
+        $('#cpwd').text("");
+
+        $('#firstnameInput').text("");
+        $('#lastnameInput').text("");
+        $('#emailInput').text("");
+        $('#pwdInput').text("");
+        $('#cpwdInput').text("");
+    }
+
+    $('#clearButton').on('click', function () {
+        clearAll()
+    })
+
+    $('#verify').on('click', function () {
+        var email = $('#email').val();
+        var data = {
+            email: email
+        }
+        axios.get('http://localhost:3000/api/user/emailVerified', data)
+            .then(function (response) {
+                alert('Congrats,you have verified your email!');
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+
+        return false;
+    })
+
+    $("#signinbtn").on("click", function () {
+        var email = $("#email1").val();
+        var pwd = $("#pwd1").val();
+
+        var emailInput = $("#emailInput1");
+        var pwdInput = $("#pwdInput1");
+
+        if (!email || !pwd) {
+            if (!email) {
+                emailInput.text("Filed must not be empty");
+            }
+            if (!pwd) {
+                pwdInput.text("Filed must not be empty");
+            }
+            return false;
+        }
+
+        const data = {
+            email: email,
+            pwd: pwd
+        }
+        axios.post('http://localhost:3000/api/user/signin', data)
+            .then(function (response) {
+                
+                if (response.data['success']) {
+                    var token = response.data['token'];
+                    localStorage.setItem('token', token);
+                    window.location.href = "User.html";
+                } else {
+                    pwdInput.text('Please check your account and password');
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    });
+
 });
+
