3fd14729a6bf63d572703c4be0ea95f93fd74ecd
s4602249 <xzhe0846@uni.sydney.edu.au>
2022-05-16 14:08:38 +1000

    minor change

--- server/models/phone.js
+++ server/models/phone.js
@@ -69,18 +69,9 @@ phoneSchema.statics.searchPhoneList = function (param, callback) {
     var query = {};
     
     query.disabled = { $exists: false };
-    //param.title="4G-Unlocked Huawei Honor 6 5.0 TFT LTPS Screen 1920 x 1080 pixel 13.0MP Camare Android 4.4.2 Kirin 920 Octa-core Smartphone 3GB RAM+16GB ROM Dual SIM (white) - International Version No Warranty";
-    //param.title="4G-Unlocked Huawei Honor 6 5.0 TFT LTPS Screen 1920 x 1080 pixel 13.0MP Camare Android 4.4.2 Kirin 920 Octa-core Smartphone 3GB RAM 16GB ROM Dual SIM (white) - International Version No Warranty"
-    //"CLEAR CLEAN ESN" Sprint EPIC 4G Galaxy SPH-D700*FRONT CAMERA*ANDROID*SLIDER*QWERTY KEYBOARD*TOUCH SCREEN
-    //param.title="4G-Unlocked Huawei Honor 6 5.0 TFT LTPS Screen 1920 x 1080 pixel 13.0MP Camare Android 4.4.2 Kirin 920 Octa-core Smartphone 3GB RAM+16GB ROM Dual SIM (white) - International Version No Warranty"
-    //             4G-Unlocked Huawei Honor 6 5.0 TFT LTPS Screen 1920 x 1080 pixel 13.0MP Camare Android 4.4.2 Kirin 920 Octa-core Smartphone 3GB RAM 16GB ROM Dual SIM (white) - International Version No Warranty
-    // (),\,+ *,&
-    // (),\,  *,&
-    //%20(),\,+%20*,%26
     // (),\,+ *,&
     //console.log("xxx para titile is "+param.title)
     if (param.title) {
-        //let data = "`test@#$demo%^here[sf.s]\|demo{delata}*testing?+$^()`"
         let spcial=[ "*", "?", "^", "$", "(", ")", "[", "]", "{", "}", "|","\"","+"]    
         for (let c of spcial) {
           if(param.title.includes(c) ) {
@@ -88,7 +79,8 @@ phoneSchema.statics.searchPhoneList = function (param, callback) {
           }
         }
         query.title = new RegExp( param.title, "i");
-        
+        console.log("XXXparam.title "+ param.title);
+        console.log("XXXquery.title "+ query.title);
         //query.title = "Samsung - Galaxy Young 2 DUOS G130 Unlocked GSM Dual-SIM + HSPA Phone - White";
     }
     if (param.brand&&param.brand != "All") {
@@ -98,7 +90,6 @@ phoneSchema.statics.searchPhoneList = function (param, callback) {
         query.price = { $lte: param.price };
     }  
   
-    //console.log("title inside  "+ query.title);
     return this
         .find(query)
         .select({ title: 1, brand: 1, image: 1, stock: 1, seller: 1, price: 1, reviews: 1, disabled: 1 })
@@ -107,7 +98,7 @@ phoneSchema.statics.searchPhoneList = function (param, callback) {
 
 phoneSchema.statics.findSoldOut = function (callback) {
     return this
-        .find({ stock: { $gt: 0 } }, { disabled: null }).sort({ stock: 1 }).limit(5)
+        .find({ stock: { $gt: 0 } , disabled:{$exists:false}}).sort({ stock: 1 }).limit(5)
         .select({ title: 1, brand: 1, image: 1, stock: 1, seller: 1, price: 1, reviews: 1, disabled: 1 })
         .exec(callback)
 }
